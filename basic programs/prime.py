3203e40acb712744e881d4d661a5a6a9b61cab53
prithvishetty925 <122429412+prithvishetty925@users.noreply.github.com>
2023-09-02 22:16:09 +0530

    Update prime.py

--- basic programs/prime.py
+++ basic programs/prime.py
@@ -1,22 +1,21 @@
-def is_prime(num):
-    if num <= 1:
+def is_prime(number):
+    if number <= 1:
         return False
-    elif num <= 3:
+    if number <= 3:
         return True
-    elif num % 2 == 0 or num % 3 == 0:
+    if number % 2 == 0 or number % 3 == 0:
         return False
     i = 5
-    while i * i <= num:
-        if num % i == 0 or num % (i + 2) == 0:
+    while i * i <= number:
+        if number % i == 0 or number % (i + 2) == 0:
             return False
         i += 6
     return True
 
-try:
-    n = int(input("Enter a number: "))
-    if is_prime(n):
-        print(f"{n} is a prime number.")
-    else:
-        print(f"{n} is not a prime number.")
-except ValueError:
-    print("Invalid input. Please enter a valid integer.")
+# Input a number to check for primality
+n = int(input("Enter a number: "))
+
+if is_prime(n):
+    print(n, "is a prime number.")
+else:
+    print(n, "is not a prime number.")
