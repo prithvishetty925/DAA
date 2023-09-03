1cbb8c72ae51a6b77739491c47950be2f8a9357d
prithvishetty925 <122429412+prithvishetty925@users.noreply.github.com>
2023-09-03 13:54:49 +0530

    Update linear sort.py

--- basic programs/linear sort.py
+++ basic programs/linear sort.py
@@ -1,16 +1,15 @@
 def linear_search(arr, target):
     for i, element in enumerate(arr):
         if element == target:
-            return i  # Return the index where the target is found
-    return -1  # Return -1 if the target is not found
+            return i  
+    return -1  
 
-# Example usage:
 my_list = [4, 2, 7, 1, 9, 5]
-target_value = 7
+n=int(input("enter the element:"))
 
-result = linear_search(my_list, target_value)
+result = linear_search(my_list, n)
 
 if result != -1:
-    print(f"Element {target_value} found at index {result}.")
+    print(f"Element found at index",result)
 else:
-    print(f"Element {target_value} not found in the list.")
+    print(f"Element not found in the list.")
