a438fec5fa6b6f98252b98c25f6c100bb497ea59
prithvishetty925 <122429412+prithvishetty925@users.noreply.github.com>
2023-09-07 13:32:08 +0530

    Create array.py

---
+++ basic programs/array.py
@@ -0,0 +1,17 @@
+# Create an array of 5 integers
+my_array = [1, 2, 3, 4, 5]
+
+# Display the array items
+print("Array items:", my_array)
+
+# Access individual elements through indexes
+for i in range(len(my_array)):
+    print(f"Element {i}: {my_array[i]}")
+
+OUTPUT:
+Array items: [1, 2, 3, 4, 5]
+Element 0: 1
+Element 1: 2
+Element 2: 3
+Element 3: 4
+Element 4: 5
