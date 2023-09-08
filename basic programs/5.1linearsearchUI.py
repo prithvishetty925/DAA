102ef9e4b2b1d5a33004991ea9c4d0fe3223a7be
prithvishetty925 <122429412+prithvishetty925@users.noreply.github.com>
2023-09-08 12:40:55 +0530

    Update 5.1linearsearchUI.py

--- basic programs/5.1linearsearchUI.py
+++ basic programs/5.1linearsearchUI.py
@@ -1,11 +1,12 @@
-def sequential_search(arr, target):
-    for i, item in enumerate(arr):
-        if item == target:
-            return i  
-    return -1  
+def linear_search(arr, target):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i  # Return the index if the target is found
+    return -1  # Return -1 if the target is not found
+
 arr = [int(x) for x in input("Enter a list of numbers separated by spaces: ").split()]
 target = int(input("Enter the number you want to search for: "))
-index = sequential_search(arr, target)
+index = linear_search(arr, target)
 
 if index != -1:
     print(f"{target} found at index {index}")
